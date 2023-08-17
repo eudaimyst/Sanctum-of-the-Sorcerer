d5ead696540649c9b3f127411f51fbc3b209d0bd
skipgamer <tpilepich@gmail.com>
2023-08-17 11:45:06 +1000

    z

--- lib/global/attack_params.lua
+++ lib/global/attack_params.lua
@@ -10,7 +10,12 @@
 	-- Define module
 	local params = {}
 
-
+    local attackAnim = { --stores the frame data for loading textures and playing animation through puppet anim system
+        pre = { frames = 3, rate = 20, loop = false },
+        windup = { frames = 4, rate = 12, loop = true },
+        main = { frames = 3, rate = 8, loop = false },
+        post = { frames = 2, rate = 10, loop = false },
+    }
     local displayTypes = {
         beam = { [1] = {segments = 1, segmentOffset = 0} },
         projectile = { [1] = {speed = 1, rotates = false, endScale = 1} },
@@ -18,6 +23,8 @@
     }
     params.default = { --default params, these are overriden when specified in params
         name = "default_attack", --checks this folder name for images/emitter
+        animation = "attack", --name of animation to play, images stored in puppets content folder
+        animData = attackAnim,
         displayType = "projectile", --"image", "emitter"
         displayParams = { [1] = {speed = 600, spins = false, endScale = 1} }, --number of images/emitters to display and their properties
         displayScale = 1, --size of attack display normalised to 64 pixels
--- lib/global/spell_params.lua
+++ lib/global/spell_params.lua
@@ -9,36 +9,61 @@
 
 	-- Define module
 	local params = {}
-
 	---------- THESE ARE FOR REFERENCE ONLY, DEFAULT VALUES FOR ATTACK.LUA TAKEN FROM ATTACK_PARAMS.LUA ------------------
     --[[
 	params.default = { --default params, these are overriden when specified in params
         name = "default_attack", --checks this folder name for images/emitter
-        displayType = "image", --"image", "emitter"
+        animation = "attack", --name of animation to play, images stored in puppets content folder
+        animData = attackAnim,
+        displayType = "projectile", --"image", "emitter" --!!whether emitters or images are used is determined by files in folder, both are attempted laoded!!
+        displayParams = { [1] = {speed = 600, spins = false, endScale = 1} }, --number of images/emitters to display and their properties
         displayScale = 1, --size of attack display normalised to 64 pixels
         element = gc.elements.fire,
         duration = 1, --lifetime of the attack
         targetType = "point", --"point", "entity", "self", "none"
         radius = -1, --circular radius of an aoe attack
+        boundAngle = 30, --limits the angle of attack with radius
 		shape = nil, --specified points of shape of an aoe attack
         maxDistance = 500, --max distance attack can travel
-        boundAngle = 30, --limits the angle of attack with radius
-        radiates = true, --attack radiates (or travels) from origin rather than appearing at target
         damage = 10, --shorthand for applying an effect (TODO: change to effect)
         cooldown = 3, --time before attack can be cast again
         range = -1, --maximum distance for attack target
         channelTime = -1, --sticks between main and post for the channel duration until cancelled
         windupTime = 0.5, --how long before the attack is fired
-        windupGlow = true, --whether or not to display a glowing emitter during windup
+        windupGlow = false, --whether or not to display a glowing emitter during windup 
     }]]
 
+    --TODO: replace rate with duration
+    local castProj = { --stores the frame data for loading textures and playing animation through puppet anim system
+        pre = { frames = 3, rate = 20, loop = false },
+        windup = { frames = 4, rate = 12, loop = true },
+        main = { frames = 3, rate = 8, loop = false },
+        post = { frames = 2, rate = 10, loop = false },
+    }
+
+    local castRaise = {
+        pre = { frames = 5, rate = 20, loop = false },
+        windup = { frames = 3, rate = 12, loop = true },
+        main = { frames = 3, rate = 8, loop = false },
+        post = { frames = 4, rate = 20, loop = false },
+    }
+
     params.fireBolt = {
+        animation = "cast_proj", --name of animation to play, images stored in puppets content folder
+        animData = castProj,
         name = "fireBolt",
         windupGlow = true,
         element = gc.elements.fire
     }
     params.iceBolt = {
         name = "iceBolt",
+        windupGlow = true,
+        element = gc.elements.ice
+    }
+    params.iceStorm = {
+        animation = "cast_raise",
+        animData = castRaise,
+        name = "iceStorm",
         windupGlow = true,
         element = gc.elements.ice
     }
